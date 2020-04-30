1cfdf9ab5e83fbd1d1202a89a89e09f3582bc592
Javad Jazaeri <60163916+A30004560@users.noreply.github.com>
2020-04-30 14:44:37 +1000

    Update and rename email_code.bat to email_code1.bat

--- email_code1.bat
+++ email_code1.bat
@@ -1,5 +1,5 @@
 call "C:\ProgramData\Anaconda3\Scripts\activate.bat"
 call pushd "\\glawi222\data$\Data\Common\New Energy\Commercial Development\Data strategy and analytics\12 vppsa-move-and-churn-check\rep1_test\"
 call activate myenv_test1
-call python -m email_code 'A30004560' "javad.jazaeri@gmail.com"
+call python -m email_code1 'A30004560' "javad.jazaeri@gmail.com"
 call popd
