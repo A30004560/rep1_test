0b8506a3181744fbf5d1868a60b838c263f51575
Javad Jazaeri <60163916+A30004560@users.noreply.github.com>
2020-04-30 11:34:30 +1000

    Update email_code1.py

--- email_code1.py
+++ email_code1.py
@@ -4,7 +4,7 @@ today = datetime.datetime.today().date()
 
 message = mailer.Message()
 
-message.From = 'jjazaeri@agl.com.au'
+message.From = 'A30004560@agl.com.au'
 message.To = ['javad.jazaeri@gmail.com, jjazaeri@agl.com.au']
 message.Subject = 'DR BYOT churned customer report {0}'.format(today)
 
