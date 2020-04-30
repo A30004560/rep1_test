3f02e0adf637331d85b6b12745885fa8488a2e24
Javad Jazaeri <60163916+A30004560@users.noreply.github.com>
2020-04-30 14:40:41 +1000

    Update email_code1.py

--- email_code1.py
+++ email_code1.py
@@ -1,19 +1,20 @@
-import mailer
-import datetime
-today = datetime.datetime.today().date()
+def job_email(user, reciever):
+  import mailer
+  import datetime
+  today = datetime.datetime.today().date()
 
-message = mailer.Message()
+  message = mailer.Message()
 
-message.From = 'A30004560@agl.com.au'
-message.To = ['javad.jazaeri@gmail.com, jjazaeri@agl.com.au']
-message.Subject = 'DR BYOT churned customer report {0}'.format(today)
+  message.From = '{user}.com.au'.format(user=user)
+  message.To = [reciever]
+  message.Subject = 'DR BYOT churned customer report {0}'.format(today)
 
-message.Body = '''Hi Team,
+  message.Body = '''Hi Team,
 
-On {dt_today}, you got this email. 
-'''.format(dt_today=today)
-#message.attach("P:/New Energy/Churn Moveout Report/Input_file/Full VPPSA Site List V3.xlsx")
+  On {dt_today}, you got this email. 
+  '''.format(dt_today=today)
+  #message.attach("P:/New Energy/Churn Moveout Report/Input_file/Full VPPSA Site List V3.xlsx")
 
-sender = mailer.Mailer('aglsmtp05.agl.com.au')
+  sender = mailer.Mailer('aglsmtp05.agl.com.au')
 
-sender.send(message)
+  sender.send(message)
